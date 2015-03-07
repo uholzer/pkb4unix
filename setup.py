ed0ad45e278d2a085dbacd101dfef259ae798bd8
Urs Holzer <urs@andonyar.com>
2015-03-08 00:13:11 +0100

    Upgrade from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,49 +1,58 @@
-#!/usr/bin/env python
-
+from setuptools import setup, find_packages
+from os import path
 import sys
-from distutils.core import setup
 
-if not sys.version_info >= (3,2):
-    print("Error: pkb4unix requires at least Python 3.2.")
+if not sys.version_info >= (3,4):
+    print("Error: pkb4unix requires at least Python 3.4.")
     exit(1)
 
-setup(name='pkb4unix',
-      version='0.1',
-      description='A personal knowledge base using RDF and SPARQL following the UNIX philosophy',
-      author='Urs Holzer',
-      author_email='urs@andonyar.com',
-      url='http://www.andonyar.com/rec/2012/pkb/',
-      packages=['PKB4Unix'],
-      scripts=[
+setup(
+    name='PKB4Unix',
+    version='0.1.0',
+    description='A personal knowledge base using RDF and SPARQL following the UNIX philosophy',
+    author='Urs Holzer',
+    author_email='urs@andonyar.com',
+    url='http://www.andonyar.com/rec/2012/pkb/',
+    license='GPLv3+',
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    keywords='semantic web knowledge base',
+    packages=['PKB4Unix'],
+    install_requires=[
+        'rdflib>=4.2.0',
+    ],
+    package_data={
+        'pkb4unix.calendar': ['ontology/*.trig'],
+    },
+    data_files=[
+        ('share/pkb4unix/templates', [
+            'templates/README',
+            'templates/task.tmpl',
+            'templates/website.tmpl',
+        ]),
+    ],
+    scripts = [
         'know',
         'know-construct',
-        'know-graph',
-        'know-load',
+        'know-convert',
+        'know-edit',
+        'know-format-csv',
         'know-merge',
-        'know-query',
-        'know-query-template',
-        'know-rdf-construct',
-        'know-rdf-edit',
-        'know-rdf-format-csv',
-        'know-rdf-uuid',
-        'know-render',
-        'know-rm',
-        'know-save',
         'know-search',
-      ],
-      keywords=['Requires: rdflib'],
-      data_files=[
-            ('share/pkb4unix/templates', [
-                'templates/README',
-                'templates/task.tmpl',
-                'templates/website.tmpl',
-            ])
-      ],
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-            "Operating System :: POSIX",
-            "Natural Language :: English",
-      ]
-     )
-
+        'know-sparql-construct',
+        'know-sparql-discover',
+        'know-sparql-graph',
+        'know-sparql-load',
+        'know-sparql-merge',
+        'know-sparql-query',
+        'know-sparql-query-template',
+        'know-sparql-render',
+        'know-sparql-rm',
+        'know-sparql-save',
+    ],
+)
